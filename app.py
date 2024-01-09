bcfdc770bebf21880c1fbb3b31a19fb8f900ea2f
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2024-01-09 09:27:52 +0700

    remove appcheck

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import ssl
 from firebase_admin import initialize_app
 from flask import Flask
 
-from middlewares.auth_middleware import verify_firebase_id_token, verify_app_check
+from middlewares.auth_middleware import verify_firebase_id_token
 from routes import register_blueprints
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -13,7 +13,7 @@ initialize_app()
 app = Flask(__name__)
 register_blueprints(app)
 app.before_request(verify_firebase_id_token)
-app.before_request(verify_app_check)
+# app.before_request(verify_app_check)
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0', port=8080)
