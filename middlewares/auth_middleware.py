4d62e859e5defff8c56d0b347d38da6a5b9ef20d
irfanakbari <irfanakbarihabibi18@gmail.com>
2024-02-12 02:11:03 +0700

    middleware response

--- middlewares/auth_middleware.py
+++ middlewares/auth_middleware.py
@@ -1,6 +1,9 @@
+from typing import Tuple
+
 import flask
 from firebase_admin import app_check
 from firebase_admin.auth import verify_id_token
+from flask import jsonify, Response
 
 
 def verify_firebase_token(token):
@@ -13,25 +16,28 @@ def verify_firebase_token(token):
         return None
 
 
-def verify_firebase_id_token() -> None:
+def verify_firebase_id_token() -> tuple[Response, int]:
     # Extract the Firebase ID token from the Authorization header
     authorization_header = flask.request.headers.get('Authorization', default="")
     if not authorization_header or not authorization_header.startswith('Bearer '):
-        flask.abort(401)
+        error_message = {"error": "Authorization header is missing or invalid. Please provide a valid access token."}
+        return jsonify(error_message), 401
     id_token = authorization_header.split(' ')[1]
 
     try:
         # Verify the Firebase ID token
         decoded_token = verify_firebase_token(id_token)
         if not decoded_token:
-            flask.abort(401)
+            error_message = {"error": "Invalid Access Token"}
+            return jsonify(error_message), 401
 
         # Add the decoded token to the request context for later use if needed
         flask.g.decoded_token = decoded_token
 
     except ValueError as e:
         print(f"Token verification failed: {e}")
-        flask.abort(500)
+        error_message = {"error": "Token verification failed. Please try again later."}
+        return jsonify(error_message), 500
 
 
 def verify_app_check() -> None:
